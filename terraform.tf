a3363ddac9444deebac0c258f9e95c3168f56ade
Denis Marcelo Santa Helena <denismst@gmail.com>
2021-04-01 17:45:42 -0300

    added new port

--- terraform.tf
+++ terraform.tf
@@ -27,6 +27,11 @@ module "sg" {
       from_port = "80"
       to_port   = "80"
     #  cidr = ["0.0.0.0/0"]
+    },
+    {
+      from_port = "443"
+      to_port   = "443"
+    #  cidr = ["0.0.0.0/0"]
     }
   ]
 }
