8cae892d54b03333bdebf521a4014f717d4a2c9b
Denis Marcelo Santa Helena <denismst@gmail.com>
2021-04-02 10:05:33 -0300

    upgrade disk

--- terraform.tf
+++ terraform.tf
@@ -32,16 +32,6 @@ module "sg" {
       from_port = "80"
       to_port   = "80"
     #  cidr = ["0.0.0.0/0"]
-    },
-    {
-      from_port = "443"
-      to_port   = "443"
-    #  cidr = ["0.0.0.0/0"]
-    },
-    {
-      from_port = "22"
-      to_port   = "22"
-    #  cidr = ["0.0.0.0/0"]
     }
   ]
 }
